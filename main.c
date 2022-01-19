e53f9c70dd64d5d2ca3c256deb06389bd264c022
jir28 <2203068000@correo.ler.uam.mx>
2022-01-18 19:09:28 -0600

    Final

--- main.c
+++ main.c
@@ -28,15 +28,60 @@ int pilaFila(TPila *pP,TFila *pF){
     return r;
 }
 
+int pilaFila2(TPila *pP, TFila *pF, TFila *ptr) {
+    int dato = 5,z,x,r;
+    AgregarAFila(pF,&dato);
+    dato = 6;
+    AgregarAFila(pF,&dato);
+    dato = 9;
+    AgregarAFila(pF,&dato);
+    dato = 0;
+    AgregarAFila(pF,&dato);
+    dato = 7;
+    AgregarAFila(pF,&dato);
+    dato = 5;
+    AgregarAFila(pF,&dato);
+    dato = 0;
+    AgregarAFila(pF,&dato);
+    dato = 2;
+    AgregarAFila(pF,&dato);
+    dato = 6;
+    AgregarAFila(pF,&dato);
+
+    while (FilaVacia(*pF)!=1){
+        QuitardeFila(pF,&dato);
+        x = dato;
+        if (dato==0){
+            z=0;
+            while (PilaVacia(*pP)!=1){
+                Desapilar(pP,&dato);
+                z = z + 1;
+                AgregarAFila(ptr,&z);
+            }
+        } else{
+            Apilar(pP,&x);
+        }
+    }
+    Desapilar(pP,&dato);
+    r = dato;
+    return r;
+}
+
 int main() {
     TPila S;
-    TFila Q;
-    int dato,resul;
+    TFila Q,Q1,Q2;
+    int dato,resul,res2;
 
     CrearPila(&S,sizeof(dato));
     CrearFila(&Q,sizeof(dato));
-
     resul = pilaFila(&S,&Q);
     printf("El numero tope de la pila es: %d",resul);
+
+    CrearFila(&Q1,sizeof (dato));
+    CrearFila(&Q2,sizeof (dato));
+    CrearPila(&S, sizeof(dato));
+    res2 = pilaFila2(&S, &Q1, &Q2);
+    printf("\nEl valor de la pila al final de la 2 es: %d",res2);
+
     return 0;
 }
